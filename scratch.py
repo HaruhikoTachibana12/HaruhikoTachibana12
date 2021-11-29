c147f8bc6ae47a8916845f904de4332a7638e5e2
HaruhikoTachibana12 <95222711+HaruhikoTachibana12@users.noreply.github.com>
2021-11-29 19:57:26 +0800

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,6 @@
+print(" Welcome to Ohm's Law Calculator ")
+
+print( "Disclaimer: To avoid misunderstanding with units and inaccurate calculations. The SI unit is an international system of measures used internationally in technical and scientific studies. Any invalid S.I unit that would result in incorrect calculations will be out of responsibility by the admin. Thank you")
+
+print("Choose what do you want to calculate: Voltage{V}, Current {A}, Resistance {Ω}")
+    choosevir = input("Enter the represented SI Unit:   ")
